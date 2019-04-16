89b1ab5f3a74f4d49894ed04b3c29d10139b0924
sjtype <31806705+sjtype@users.noreply.github.com>
2019-04-16 19:34:30 -0400

    added Bootstrap

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,1 +1,11 @@
-/* Custom JavaScript goes here */
+// IIFE -- Immediately Invoked Function Expression
+(function(){
+
+    function Start() {
+        console.log('%c App Started...', 
+        "font-size: 20 px; color: blue; font-weight: bold;")
+
+    }
+
+    window.addEventListener("load", Start);
+})();
