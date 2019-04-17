c2946cbaae9cfc66b1575036012078e2d0adb7ff
sjtype <31806705+sjtype@users.noreply.github.com>
2019-04-16 23:04:03 -0400

    displayed db in view

--- config/db.js
+++ config/db.js
@@ -1,4 +1,5 @@
 // think of a module as a container
 module.exports = {
-    "URI" : "mongodb://localhost/test"
+    //"URI" : "mongodb://localhost/test"
+    "URI" : "mongodb+srv://user:password0@cluster0-tabvi.azure.mongodb.net/test?retryWrites=true"
 }
